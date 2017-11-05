6a7caa4929706cea6feaeaa351e652749fe42eb5
Srivatsa S <srivatsashankaran@gmail.com>
2017-11-05 23:01:27 +0530

    Implementing the two way data binding by using ngModel from the FormsModule and editing the Hero name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,12 @@ export class Hero {
   //templateUrl: './app.component.html',
   template: `<h1>{{title}}</h1>
              <h2>{{hero.name}} details!</h2>
-             <div><label>id: </label>{{ hero.id }}</div>
-             <div><label>name: </label>{{ hero.name }}</div>`,
+              <div><label>id: </label>{{ hero.id }}</div>
+               <div>
+                <label>name: </label>
+                <input [(ngModel)]="hero.name" placeholder="name">
+               </div>
+             `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
